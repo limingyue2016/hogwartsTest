2a512b2b01a9dede237dbd1caddf5da72d0f2f71
李明月 <limingyue5@xiaomi.com>
2020-11-11 14:46:33 +0800

    配置allure
    Signed-off-by: 李明月 <limingyue5@xiaomi.com>

--- src/test/java/junit5Test/CalculatorTest.java
+++ src/test/java/junit5Test/CalculatorTest.java
@@ -1,32 +1,39 @@
 package junit5Test;
 
-import junit5Test.BaseCase;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//@RunWith(JUnitPlatform.class)
+//@RunWith(JunitPlatform.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DisplayName("DisplayName")
+@Epic("计算器测试用例")
+@Feature("计算器功能测试用例")
+@Link("https://github.com/")
+@Link(name = "github", type = "mylink")
 public class CalculatorTest extends BaseCase {
     private Calculator calculator = new Calculator();
 
     @Test
-    @Disabled
+    @Severity(SeverityLevel.BLOCKER)
     void calculatorTest() {
         assertAll("testCalculator",
                 () -> assertEquals(calculator.add(1, 1), 2),
                 () -> assertEquals(calculator.subtract(1, 1), 0),
                 () -> assertEquals(calculator.multiply(1, 1), 1),
-                () -> assertEquals(calculator.divide(1, 1), 1),
-                () -> assertEquals(calculator.count, 4));
+                () -> assertEquals(calculator.divide(1, 1), 1));
 
     }
 
     @Test
     @Order(4)
     @Tag("calculator")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("description")
+    @Story("加法")
     void addTest() {
         assertEquals(calculator.add(1, 1), 2);
     }
@@ -34,19 +41,23 @@ public class CalculatorTest extends BaseCase {
     @Test
     @Order(3)
     @Tag("calculator")
+    @Story("减法")
     void subtractTest() {
         assertEquals(calculator.subtract(1, 1), 0);
     }
 
-    @Test
     @Order(2)
     @RepeatedTest(3)
+    @Story("乘法")
     void multiplyTest() {
         assertEquals(calculator.multiply(1, 1), 1);
     }
 
     @Test
     @Order(1)
+    @Issue("Issue1")
+    @Story("除法")
+    @TmsLink("https://github.com/")
     void divideTest() {
         assertEquals(calculator.divide(1, 1), 1);
     }
