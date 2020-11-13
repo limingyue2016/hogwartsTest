767db6812a268309b34e1f003b14be986647bea9
limingyue5 <limingyue5@xiaomi.com>
2020-11-13 22:18:22 +0800

    通过cookie登录企业微信
    Signed-off-by: limingyue5 <limingyue5@xiaomi.com>

---
+++ src/test/java/seleTest/weixinTest.java
@@ -0,0 +1,57 @@
+package seleTest;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 1 selenium环境搭建 done
+ * 2 微信登录 done
+ */
+public class weixinTest extends BaseCase{
+    public String url = "https://work.weixin.qq.com/wework_admin/frame";
+
+    @Test
+    void loginTest() {
+        driver.get(url);
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 获取登录的cookies
+        Set<Cookie> cookies = driver.manage().getCookies();
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        try {
+            mapper.writeValue(new File("cookies.yaml"), cookies);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void reloginTest() throws IOException {
+        driver.get(url);
+
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        TypeReference typeReference = new TypeReference<List<HashMap<String, Object>>>() {};
+        // 文件中读取cookies
+        List<HashMap<String, Object>> cookies = mapper.readValue(new File("cookies.yaml"), typeReference);
+        // 登录信息中添加cookies
+        cookies.forEach(cookieMap->{
+            driver.manage().addCookie(new Cookie(cookieMap.get("name").toString(), cookieMap.get("value").toString()));
+        });
+
+        //刷新页面
+        driver.navigate().refresh();
+    }
+}
