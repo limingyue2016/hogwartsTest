d9c6071b4f03df99643ca57b636de43cc372445f
李明月 <limingyue5@xiaomi.com>
2020-12-11 17:35:28 +0800

    增加wiremock测试用例
    Signed-off-by: 李明月 <limingyue5@xiaomi.com>

--- README.md
+++ README.md
@@ -5,20 +5,24 @@
 #### src
 
 -  junit5
--  wework.core
--  wework.app
--  wework.web
--  wework.framework
+-  wework
+    - core
+    - app
+    - web
+    - framework
 
 #### test
 
 -  junit5
--  wework.app
--  wework.web
--  wework.framework
+-  wework
+    - app
+    - web
+    - framework
+-  wiremock
 
 ## 知识点
 
+### 客户端自动化测试
 - junit5环境搭建，基础注解使用，assertAll()，并发测试，参数化
 - allure基本使用
 - selenium环境搭建
@@ -26,3 +30,6 @@
 - PO模式搭建测试框架
 - 测试数据数据驱动
 - 测试步骤数据驱动
+
+### 服务端自动化测试
+- wiremock
---
+++ src/test/java/com/wiremock/WiremockTest.java
@@ -0,0 +1,69 @@
+package com.wiremock;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+public class WiremockTest {
+    @BeforeAll
+    static void init() {
+        WireMockServer wireMockServer = new WireMockServer(wireMockConfig().port(8089)); //No-args constructor will start on port 8080, no HTTPS
+        wireMockServer.start();
+        // If you’ve changed the port number and/or you’re running the server on another host, you’ll need to tell the client
+        configureFor("127.0.0.1", 8089);
+    }
+
+    @Test
+    public void stubMock() throws InterruptedException {
+        stubFor(get(urlEqualTo("/test"))
+                .withHeader("Accept", equalTo("text/xml"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("test")));
+
+        Thread.sleep(500000);
+    }
+
+    @Test
+    public void easyMock() throws InterruptedException {
+        stubFor(get(urlEqualTo("/test"))
+                .withHeader("Accept", equalTo("text/xml"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("test")));
+
+        Thread.sleep(10000);
+
+        reset();
+        stubFor(get(urlEqualTo("/test"))
+                .withHeader("Accept", equalTo("text/xml"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("test_01")));
+
+        Thread.sleep(500000);
+    }
+
+    @Test
+    public void proxyMock() throws InterruptedException, URISyntaxException, IOException {
+        stubFor(get(urlMatching(".*")).atPriority(10)
+                .willReturn(aResponse().proxiedFrom("https://ceshiren.com")));
+
+        stubFor(get(urlEqualTo("/categories_and_latest")).atPriority(10)
+                .willReturn(aResponse().withBody(Files.readAllBytes(Paths.get(WiremockTest.class.getResource("/mock.json").toURI())))));
+
+        Thread.sleep(500000);
+    }
+}
+
